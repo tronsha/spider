56c289856858a8c1634d1b0fd4ff8dbfa635ebc8
Stefan Hüsges <tronsha@gmx.de>
2018-03-29 11:58:33 +0200

    Update spider

--- src/Spider.php
+++ src/Spider.php
@@ -43,11 +43,7 @@ class Spider
     {
         $this->client->setMethod(Client::GET);
         var_dump($this->client->setUrl('http://www.heise.de')->request()->getHeader());
-        var_dump($this->client->setUrl('http://www.heise.de')->request()->getHeader('content-type'));
         var_dump($this->client->setUrl('http://www.golem.de')->request()->getHeader());
         var_dump($this->client->setUrl('http://www.gnuterrypratchett.com')->request()->getHeader());
-        var_dump($this->client->getRequestHeader('user-agent'));
-        var_dump((new Robots())->isDisallow('http://www.heise.de/foo.html'));
-        var_dump((new Robots())->isDisallow('https://www.facebook.com'));
     }
 }
